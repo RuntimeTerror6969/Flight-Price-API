5610beb5c36bb6f8f5ed761d77d81d500d62a0d7
adarsh <aadarsh.22.2003@gmail.com>
2025-01-05 14:45:33 +0530

    Add src/index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Replace "*" with your frontend URL for production
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/api", flightRoutes);
 app.use("/api/auth", authRoutes);
